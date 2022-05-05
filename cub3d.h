034981fcf098c779169c84da13566a067f8a4edd
Mariia <masya.kim@mail.ru>
2022-05-05 19:32:57 +0300

    add other files

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: a79856 <a79856@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 03:07:25 by a79856            #+#    #+#             */
-/*   Updated: 2022/05/05 18:51:17 by a79856           ###   ########.fr       */
+/*   Updated: 2022/05/05 19:28:20 by a79856           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "minilibx_mms_202002199/mlx.h"
+
+# define PI					3.141592653589793
+# define MINI_TILE_SIZE		10
+
+struct s_window;
 
 typedef struct s_info
 {
@@ -35,7 +40,7 @@ typedef struct s_info
 	int				map_height;
 	char			*first_line;
 	int				nb_sprites;
-	struct s_total	*total;
+	struct t_window	*total;
 }				t_info;
 
 typedef struct	s_data {
@@ -48,6 +53,9 @@ typedef struct	s_data {
 
 typedef struct s_player
 {
+	double	dir_x;
+	double	dir_y;
+	char	orientation;
 	int		pos_map_x;
 	int		pos_map_y;
 	double	pos_pix_x;
@@ -56,14 +64,22 @@ typedef struct s_player
 	int		mov_down;
 	int		mov_left;
 	int		mov_right;
-	char	orientation;
-}	t_play;
+	int		rot_left;
+	int		rot_right;
+	int		turn_direction;
+	int		axis_direction;
+	int		lat_direction;
+	double	rotation_angle;
+	double	move_speed;
+	double	rotation_speed;
+	int		mouse_moving;
+}				t_player;
 
 typedef struct s_window
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	t_play	*play;
+	t_player	*play;
 	t_data	*img;
 	t_info	*info;
 }	t_window;
--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: a79856 <a79856@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 03:14:00 by a79856            #+#    #+#             */
-/*   Updated: 2022/05/05 18:56:36 by a79856           ###   ########.fr       */
+/*   Updated: 2022/05/05 19:30:29 by a79856           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
 
 ////////кастомная mlx_put
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -56,7 +57,7 @@ int	is_a_key(int key)
 
 // 0 - A S -1 W -13 D - 2
 
-void	buttons(int key, t_play *data)
+void	buttons(int key, t_player *data)
 {
 	if (key == 13)
 		data->pos_map_y -= 8;
@@ -84,69 +85,10 @@ int	deal_key(int key, t_window *data)
 	return (0);
 }
 
-///////////////////////////////////
-
-void	ft_initialiser_ceil_and_floor(t_window *total)
-{
-	total->mlx_ptr = 0;
-	total->win_ptr = 0;
-	total->info->total = total;
-	total->info->floor[0] = -1;
-	total->info->floor[1] = -1;
-	total->info->floor[2] = -1;
-	total->info->ceiling[0] = -1;
-	total->info->ceiling[1] = -1;
-	total->info->ceiling[2] = -1;
-}
-
-int	ft_initialiser_info(char **av, t_window *total)
-{
-	int	i;
-
-	ft_bzero(total->info, sizeof(t_info));
-	total->info->av = av[1];
-	ft_initialiser_ceil_and_floor(total);
-	ft_bzero(total->player, sizeof(t_player));
-	return (0);
-}
-
-//проверка на расширение .куб
-int	ft_check_cub(char *av)
-{
-	int	size;
-
-	size = ft_strlen(av);
-	if ((size < 5) || (av[size - 1] != 'b') || (av[size - 2] != 'u')
-		|| (av[size - 3] != 'c') || (av[size - 4] != '.'))
-		return (ft_write_error("The map must have a .cub extension.\n",
-				0, NULL));
-	return (0);
-}
-
-int	ft_handle_map(char **av, t_info *info, t_window *total)
-{
-	int	fd;
-
-	if (ft_initialiser_info(av, total))
-		return (1);
-	if (ft_check_cub(av[1]) == 1)
-		return (1);
-	fd = ft_open(av[1]);
-	if (fd == 0)
-		return (ft_write_error("Open failed.\n", 1, info));
-	if (ft_get_info(fd, info, 1))
-		return (ft_free(info));
-	if (ft_check_original_map(info))
-		return (ft_free(info));
-	ft_clean_map(info);
-	close(fd);
-	return (0);
-}
-
-int main()
+int main(int ac, char **av)
 {
 	t_window wd;
-	t_play	play;
+	t_player	play;
 	t_data	img;
 	t_info	info;
 
@@ -154,8 +96,8 @@ int main()
 	play.pos_map_y = 300;
 	wd.info = &info;
 	wd.play = &play;
-	if (ft_handle_map(av, wd.info, &wd))
-		return (0);
+	// if (ft_handle_map(av, wd.info, &wd))
+	// 	return (0);
 	wd.win_ptr = mlx_init();
 	wd.img = &img;
 	wd.mlx_ptr = mlx_new_window(wd.win_ptr,512, 512, "Cub3D");
